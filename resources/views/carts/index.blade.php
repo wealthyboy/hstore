02885e5574f3d4f50deb74fb9fbda5a25b43d149
Jacob Atam <jacob.atam@gmail.com>
2020-12-19 11:49:06 +0100

    Upadated cart

--- resources/views/carts/index.blade.php
+++ resources/views/carts/index.blade.php
@@ -6,34 +6,7 @@
  
 @section('content')
  
-<div class="top-notice text-white bg--primary cart-page">
-    <div class="container-fluid text-center">
-        <div class="row">
-            
-            <div class="col-12">
-                <h5 class="d-inline-block color--light text-uppercase  mb-0"><b><i class="fas fa-money-check"></i>
-                    LAUCH WEEK  GET 5% OFF  USE  HSLNCH </b>
-                </h5>
-            </div>
-            
 
-        </div>
-        
-    </div><!-- End .container -->
-</div>
-
-<section class="breadcrumb no-banner cart-page justify-content-center">
-    <div class="breadcrumb-content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12  text-center border-bottom">
-                    >
-                    <h1 class="breadcrumb-title">Your Cart</h1>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
   
    <!--Content-->
    
