7a6b5d7e65b6c9362739116f8ad73576467a1a25
Jacob Atam <jacob.atam@gmail.com>
2020-12-20 05:03:43 +0100

    Upadated cart

--- resources/views/carts/index.blade.php
+++ resources/views/carts/index.blade.php
@@ -47,7 +47,7 @@
                 <img src="{{ $system_settings->logo_path() }}" height="110" width="80" alt="The Luxury sale Logo">
             </div>        
         </div>
-        <cart-summary :sub_total="{{ $sub_total }}" :crts="{{ $carts }}" />
+        <cart-summary  />
 
     </div>
 </section>
