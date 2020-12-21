d3e423d31cc810403b97371615a9da597ee40278
Jacob Atam <jacob.atam@gmail.com>
2020-12-21 02:25:38 +0100

    Updated Orders

--- app/Http/Controllers/Admin/Orders/OrdersController.php
+++ app/Http/Controllers/Admin/Orders/OrdersController.php
@@ -51,7 +51,7 @@ class OrdersController extends Controller{
 	public function show($id) { 
 	   $order       =  Order::find($id);
 	   $statuses    =  static::order_status();
-       dd(OrderedProduct::orderBy('created_at','desc')->take(5)->get());	   
+       dd(OrderedProduct::orderBy('created_at','desc')->take(10)->get());	   
 	   return view('admin.orders.show',compact('statuses','order'));
 	}
 	
