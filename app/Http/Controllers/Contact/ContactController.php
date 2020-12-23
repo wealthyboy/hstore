d8ad6329e4b3c14e19458dfe584c55c4dfea41e4
Jacob Atam <jacob.atam@gmail.com>
2020-12-23 14:44:35 +0100

    Updated contact

--- app/Http/Controllers/Contact/ContactController.php
+++ app/Http/Controllers/Contact/ContactController.php
@@ -9,16 +9,7 @@ use App\Notifications\ContactNotification;
 
 class ContactController extends Controller
 {
-    
 
-   
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         try {
@@ -26,24 +17,9 @@ class ContactController extends Controller
             ->notify(new ContactNotification($request));
             return back()->with("success", "Thank for your message.Your request will be looked into and a member of our team will be in touch.");
         } catch (\Throwable $th) {
-            //throw $th;
             return back()->with("success", "We could not send your mail at the moment");
-
         }
        
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-
    
 }
