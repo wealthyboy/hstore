58dd78eaedc9216ea377b2dd2982bd257047f43f
Jacob Atam <jacob.atam@gmail.com>
2021-01-08 23:51:02 +0100

    Updated alt

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -8,7 +8,7 @@
             <div class="loading">
                 <div class="loader"></div>
             </div>
-            <img src="{{ $system_settings->logo_path() }}" height="110" width="80" alt="The Luxury sale Logo">
+            <img src="{{ $system_settings->logo_path() }}" height="110" width="80" alt="Hautesignatures sale Logo">
         </div>        
     </div>
       
