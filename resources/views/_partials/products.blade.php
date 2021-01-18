28e0375a2dd52e43700858ed22a6cdce1b98e5cc
Jacob Atam <jacob.atam@gmail.com>
2021-01-18 21:00:14 +0100

    Removed wrong filters

--- resources/views/_partials/products.blade.php
+++ resources/views/_partials/products.blade.php
@@ -13,6 +13,7 @@
                 <img src="{{ $product->image_to_show_m }}" alt="{{ $product->product_name }}" />
             </a>
             <div class="btn-icon-group"></div>
+            <!-- <a href="#" data-toggle="modal" data-target="#exampleModalCenter" class="btn-quickvie" title="Quick View">Quick View</a>  -->
         </figure>
         
         <div class="product-details">
@@ -62,6 +63,27 @@
         </div>
     </div>
 @endif
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
    
 
