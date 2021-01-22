e6a6caf10135e03ad2329b771d398c3b2d4ff836
Atam Jacob <jacob.atam@gmail.com>
2021-01-22 12:48:48 -0800

    Updated text notifictions

--- app/Http/Controllers/Admin/Orders/OrdersController.php
+++ app/Http/Controllers/Admin/Orders/OrdersController.php
@@ -67,7 +67,7 @@ class OrdersController extends Controller{
 	
 	public function updateOrderStatus(Request $request){
 		//return $request->all();
-		
+		dd(true);
 		Notification::route('nexmo', '+2348063045041')
             ->notify(new OrderStatusNotification($request));
 	}
