07abc51f76e20d3b574fe0e4c2638227a2496de7
Atam Jacob <jacob.atam@gmail.com>
2021-04-17 09:05:15 -0700

    updated Show

--- resources/views/products/size.blade.php
+++ resources/views/products/size.blade.php
@@ -10,10 +10,7 @@
                 <div class="col-12  text-center border-bottom">
                     <nav aria-label="breadcrumb" class="breadcrumb-nav breadcrumb-link">
                         <div class="container d-flex justify-content-center">
-                            <!-- <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="/"><i class="icon-home"></i></a></li>
-                                <li class="breadcrumb-item active" aria-current="page">{{ $breadcrumb }}</li>
-                            </ol> -->
+                            
                         </div>
                     </nav>
                     <h1 class="breadcrumb-title">Size Guide</h1>
