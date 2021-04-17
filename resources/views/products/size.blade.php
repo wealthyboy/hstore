2187708b1ba46eaac25d7e3abb7b0741567eafc5
Atam Jacob <jacob.atam@gmail.com>
2021-04-17 09:04:39 -0700

    updated Show

--- resources/views/products/size.blade.php
+++ resources/views/products/size.blade.php
@@ -3,8 +3,25 @@
 @section('content')
 @include('_partials.top_banner')
 
-
-
+<section class="breadcrumb no-banner  justify-content-center">
+    <div class="breadcrumb-content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12  text-center border-bottom">
+                    <nav aria-label="breadcrumb" class="breadcrumb-nav breadcrumb-link">
+                        <div class="container d-flex justify-content-center">
+                            <!-- <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="/"><i class="icon-home"></i></a></li>
+                                <li class="breadcrumb-item active" aria-current="page">{{ $breadcrumb }}</li>
+                            </ol> -->
+                        </div>
+                    </nav>
+                    <h1 class="breadcrumb-title">Size Guide</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 
 <div class="container-fluid mb-3">
