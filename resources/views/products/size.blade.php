6c51de46ba16e54f8c7f50f3d2a0a303441de3bf
Atam Jacob <jacob.atam@gmail.com>
2021-04-17 10:56:29 -0700

    updated Show

--- resources/views/products/size.blade.php
+++ resources/views/products/size.blade.php
@@ -24,9 +24,9 @@
 <div class="container-fluid mb-3">
     <div  class="row">
         <div class="col-12">
-            <img src="/images/sizes/SIZE-GUIDE-1.jpg" />
             <img src="/images/sizes/SIZE-GUIDE-2.jpg" />
             <img src="/images/sizes/SIZE-GUIDE-3.jpg" />
+            <img src="/images/sizes/SIZE-GUIDE-1.jpg" />
             <img src="/images/sizes/SIZE-GUIDE-4.jpg" />
         </div>
     </div>
