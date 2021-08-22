726127d382d9ad07dbbb7191624a4981330584c2
Atam Jacob <jacob.atam@gmail.com>
2021-08-22 12:24:36 -0700

    Updated Blog

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,6 @@ use App\Product;
 use App\Category;
 use App\Review;
 use App\Information;
-use Stevebauman\Location\Location;
 use App\Currency;
 use App\SystemSetting;
 use App\Http\Helper;
@@ -31,14 +30,13 @@ class HomeController extends Controller
         $products = Product::where('featured',1)->orderBy('created_at','DESC')->take(8)->get();
         $reviews  = Review::orderBy('created_at','DESC')->take(4)->get();
         $posts  =   Information::orderBy('created_at','DESC')->where(['blog'=>true,'is_active' => true])->take(6)->get();
-        $page_title = 'HauteSignatures  ';
             
         if ( empty($site_status->make_live) ) {
-            return view('index',compact('banners','page_title','reviews','products','posts'));
+            return view('index',compact('banners','reviews','products','posts'));
         } else {
             //Show site if admin is logged in
             if ( auth()->check()  && auth()->user()->isAdmin()){
-                return view('index',compact('banners','page_title','reviews','products','posts'));
+                return view('index',compact('banners','reviews','products','posts'));
             }
             return view('underconstruction.index');
         }
