c561d9ef014dbf9e9e582d258f4985d09734f3a1
Atam Jacob <jacob.atam@gmail.com>
2021-08-25 05:59:02 -0700

    Updated image

--- app/Http/Controllers/Admin/Image/ImagesController.php
+++ app/Http/Controllers/Admin/Image/ImagesController.php
@@ -144,7 +144,7 @@ class ImagesController extends Controller
                 return $path = asset('images/'. $request->folder .'/'.$file);
             }
 
-            $img  = \Image::make($path)->fit(305, 305)->save(
+            $img  = \Image::make($path)->fit(465, 465)->save(
                 public_path('images/'. $request->folder .'/m/'.$file)
             );
             $canvas = \Image::canvas(106, 145);
