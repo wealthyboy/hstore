7d3a32e5b3d82e18f4ba2e97b643113923c69b7f
Atam Jacob <jacob.atam@gmail.com>
2021-08-25 07:30:01 -0700

    Updated image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,18 +59,28 @@
 	gtag('js', new Date());
 
 	gtag('config', 'UA-204322224-1');
-	</script>
 
-    <script>
-		Window.user = {
-			user: {!! auth()->check() ? auth()->user() : 0000 !!},
-			loggedIn: {!! auth()->check() ? 1 : 0 !!},
-			settings: {!! isset($system_settings) ? $system_settings : '' !!},
-			token: '{!! csrf_token() !!}'
-		}
+	(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-TBZPGWQ');
+
+	Window.user = {
+		user: {!! auth()->check() ? auth()->user() : 0000 !!},
+		loggedIn: {!! auth()->check() ? 1 : 0 !!},
+		settings: {!! isset($system_settings) ? $system_settings : '' !!},
+		token: '{!! csrf_token() !!}'
+	}
 	</script>
 </head>
 <body class="">
+
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBZPGWQ"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+
 	<div id="app" class="page-wrapper">
 		
 
