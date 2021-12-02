6559fb7ee9f291a893b68aeb9c19c0d01a72cd16
Atam Jacob <jacob.atam@gmail.com>
2021-12-01 20:18:37 -0800

    Added enable disable blog

--- resources/views/_partials/top_banner.blade.php
+++ resources/views/_partials/top_banner.blade.php
@@ -1,15 +1,16 @@
 <div style="background-color: {{ $global_promo->background_color }}" class="top-notice text-white">
     <div class="container-fluid text-center">
-        <div class="row">
-            @foreach($global_promo->promo_texts as $promo_text)
-            <div class="col-12 border-right ">
-                <h5 class="d-inline-block color--light text-uppercase  mb-0"><b><i class="fas fa-money-check"></i>
-                {{ $promo_text->promo}}</b>
-                </h5>
-            </div>
-            @endforeach
-            
+       <div class="offer-hignlight ">
 
+                @foreach($global_promo->promo_texts as $promo_text)
+                <div class="col-12 text-center">
+                    <h5 class="d-inline-block color--light text-uppercase  mb-0"><b>
+                    {{ $promo_text->promo}}</b>
+                    </h5>
+                </div>
+                @endforeach
+            
         </div>
     </div><!-- End .container -->
 </div>
+
