b2b7b5f654b59d734701f3334bf20165efd3efd6
Atam Jacob <jacob.atam@gmail.com>
2022-01-14 03:55:43 -0800

    Updated php

--- app/Http/Controllers/Products/ProductsController.php
+++ app/Http/Controllers/Products/ProductsController.php
@@ -89,6 +89,8 @@ class ProductsController extends Controller
 
         $products = ProductVariation::whereNotNull('name')
             ->where('allow',true)
+            ->where('quantity','>=', 1)
+
             ->filter($request,$this->filters($category_attributes))
             ->latest()
             ->paginate($this->settings->products_items_per_page);        
