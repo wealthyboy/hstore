cd949eea1856d0d23c721d4ed71e84a7412f99dd
Atam Jacob <jacob.atam@gmail.com>
2022-01-25 18:32:58 -0800

    Updated review

--- routes/web.php
+++ routes/web.php
@@ -105,14 +105,14 @@ Route::group(['middleware' => 'admin','prefix' => 'admin'], function(){
 
 
 
-Route::get('/mailable', function () {
-    $order = App\Order::find(201);
-    $settings =  App\SystemSetting::first();
-    // $total = Illuminate\Support\Facades\DB::table('ordered_product')->select(\DB::raw('SUM(ordered_product.price*ordered_product.quantity) as items_total'))->where('order_id',$order->id)->get();
-    // $sub_total = $total[0]->items_total ?? '0.00';
-    return  new App\Mail\ReviewMail($order);
+// Route::get('/mailable', function () {
+//     $order = App\Order::find(201);
+//     $settings =  App\SystemSetting::first();
+//     // $total = Illuminate\Support\Facades\DB::table('ordered_product')->select(\DB::raw('SUM(ordered_product.price*ordered_product.quantity) as items_total'))->where('order_id',$order->id)->get();
+//     // $sub_total = $total[0]->items_total ?? '0.00';
+//     return  new App\Mail\ReviewMail($order);
 
-});
+// });
 
 
 
