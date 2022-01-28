a887774c3838b2f8391102d22f73d2a394598a05
Atam Jacob <jacob.atam@gmail.com>
2022-01-28 04:46:21 -0800

    Updated customers

--- app/Http/Controllers/Admin/Orders/OrdersController.php
+++ app/Http/Controllers/Admin/Orders/OrdersController.php
@@ -51,11 +51,11 @@ class OrdersController extends Controller{
 		];
 	}
 
-	public function show($id) { 
-	   $order     =  Order::find($id);
+	public function show($id) 
+	{ 
+	   $order      =  Order::find($id);
 	   $statuses   =  static::order_status();
-	   $sub_total = $this->subTotal($order);
-
+	   $sub_total  = $this->subTotal($order);
 	   return view('admin.orders.show',compact('statuses','order','sub_total'));
 	}
 
@@ -64,14 +64,12 @@ class OrdersController extends Controller{
 	{
 		$total = \Db::table('ordered_product')->select(\DB::raw('SUM(ordered_product.price*ordered_product.quantity) as items_total'))->where('order_id',$order->id)->get();
         return $sub_total = $total[0]->items_total ?? '0.00';
-	   
 	}
 	
 	public function updateStatus(Request $request){
 		$ordered_product = OrderedProduct::findOrFail($request->ordered_product_id);
 		$ordered_product->status = $request->status;
 		$ordered_product->save();  
-
 		return $ordered_product;
 	}
 	
@@ -79,7 +77,7 @@ class OrdersController extends Controller{
 	public function updateOrderStatus(Request $request){
 		$user = User::find($request->id);
 		Notification::route('mail', $user->email)
-            ->notify(new OrderStatusNotification($user,$request));
+            ->notify(new OrderStatusNotification($user, $request));
 	}
 
 
