a4113e80ec5a7097d707e43fee428e8456381eac
Atam Jacob <jacob.atam@gmail.com>
2022-04-22 14:21:08 -0700

    Updated

--- app/Http/Controllers/Admin/Account/AccountsController.php
+++ app/Http/Controllers/Admin/Account/AccountsController.php
@@ -98,7 +98,7 @@ class AccountsController extends Controller
     
         $total_value = array_sum($total_value);
 
-        $remaining_products =  ProductVariation::where('quantity', '>=', 1)->pluck('id','quantity')->toArray();
+        $remaining_products =  ProductVariation::where('quantity',  200000000)->pluck('id')->toArray();
         foreach ($remaining_products as $key => $value) {
             # code...
         }
