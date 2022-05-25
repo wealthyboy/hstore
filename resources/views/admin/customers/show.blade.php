2c4f680b06b425176f0fdc1fdbc8904467929f6f
Atam Jacob <jacob.atam@gmail.com>
2022-05-25 12:43:31 -0700

    Updated Customers

--- resources/views/admin/customers/show.blade.php
+++ resources/views/admin/customers/show.blade.php
@@ -23,8 +23,8 @@
       <div class="col-lg-6 col-md-6 col-sm-6">
          <div class="card card-stats">
             <div class="card-content">
-               <p class="category">All Orders</p>
-               <h3 class="card-title">₦{{ optional($user->orders)->count() }}</h3>
+               <p class="category">Number OF Orders</p>
+               <h3 class="card-title">{{ optional($user->orders)->count() }}</h3>
             </div>
          </div>
       </div>
