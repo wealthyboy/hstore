28cecbe2b3a4757e1e20c7b7166d72994520ac80
Seth Atam <seth.atam@gmail.com>
2022-07-08 00:36:39 +0100

    Updated nesltett

--- resources/views/admin/customers/index.blade.php
+++ resources/views/admin/customers/index.blade.php
@@ -11,8 +11,8 @@
         <div class="col-md-12">
             <div class="card">
             <div class="text-right">
-                <a href="#" rel="tooltip" title="Export" class="btn btn-info btn-simple btn-xs">
-                    Last download time {{ optional($d)->d_t }}
+                <a href="#" rel="tooltip"  class="btn btn-info btn-simple btn-xs">
+                    Last download time {{ optional($d)->d_t || '------'}}
                 </a>
                 <a href="/admin/export" rel="tooltip" title="Export" class="btn btn-danger btn-simple btn-xs">
                     Export
