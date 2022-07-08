9ea2236e2b27647d492177248eec168d06bf8412
Seth Atam <seth.atam@gmail.com>
2022-07-08 13:08:46 +0100

    U

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -75,9 +75,9 @@
                         <a href="{{ optional($related_product)->link }}">{{ optional($related_product)->product_name }}</a>
                     </h3>
                     <div class="price-box">
-                        @if (optional($related_product)->sale_price ) 
+                        @if (optional($related_product)->salePrice() ) 
                             <span class="old-price">{{ optional($related_product)->currency }}{{ number_format(optional($related_product)->price)  }}</span>
-                            <span class="product-price">{{ optional($related_product)->currency }}{{ number_format(optional($related_product)->sale_price)  }}</span>
+                            <span class="product-price">{{ optional($related_product)->currency }}{{ number_format(optional($related_product)->salePrice())  }}</span>
                         @else
                            <span title="{{ optional($related_product->variant)->default_discounted_price }} pppp" class="product-price  pppp">{{ optional($related_product)->currency }}{{ number_format(optional($related_product)->converted_price)  }}</span>
                         @endif
